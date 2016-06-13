1d93dd66850f96028dbfda0119516d7467b14a42
Vincent Pauley <vinniepauley@gmail.com>
2016-06-12 23:23:35 -0500

    pairs working

--- scripts.js
+++ scripts.js
@@ -1,6 +1,53 @@
 /* GLOBALS */
 var i;
+var index; // need two 'i's for nested array purposes
 var selectedCardCount = 0;
+
+// 24 cards, 24 numbers defined in this array, randomly assigned to 12 pairs later on
+var numberArray = [];
+
+/* push the amount of cards into the number array */
+for(i=1;i<25;i++) {
+  numberArray.push(i);
+}
+
+/* this array will store the pairs */
+var pairs = [];
+
+
+/* make twelve pairs of random numbers from the array */
+for(index=0;index<13;index++) {
+
+  /* vars used to build the arrays in each loop */
+  var pair = '';
+  var one;
+  var two;
+
+  /* grab 2 numbers from the array and push JSON object into pairs array */
+  for(i=0;i<2;i++) {
+    /* randomly assign a number from the array index */
+    var randomArrayIndex = Math.floor(Math.random() * numberArray.length);
+    // grab random number item from array
+    var randomItem = numberArray[randomArrayIndex];
+    // remove that item
+    numberArray.splice(randomArrayIndex, 1);
+    /* add to either first or second pair */
+    if (i == 0) {
+      one = randomItem;
+    } else {
+      two = randomItem;
+      pair = [one, two];
+      pairs.push(pair);
+    }
+  }
+}
+
+/* log out pairs array during development */
+console.log(pairs);
+console.log(numberArray.length);
+
+
+
 
 /* place all 24 cards on screen */
 var cardCount = 24;
